05202b4d3fcbe7689c91ffecbf9afbc29ff45200
Enrique Cuevas Garcia <enrique.cuevas@iesdonana.org>
2025-04-14 18:09:03 +0200

    Reconfiguracion de la maldita base de datos

---
+++ config/mysql.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'client' => 'mysql',
+    'connection' => [
+        'host' => '127.0.0.1',
+        'port' => 3306,
+        'database' => '''',
+        'username' => '''',
+        'password' => '''',
+        'protocol' => 'TCP',
+    ],
+]; 
