056391986b4fa62f4a5575e19f2eeac3169320a9
Enrique Cuevas Garcia <enrique.cuevas@iesdonana.org>
2025-04-15 14:16:59 +0200

    Actualizacion

--- config/database.php
+++ config/database.php
@@ -21,6 +21,8 @@ return [
             'options' => [
                 PDO::ATTR_PERSISTENT => false,
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_EMULATE_PREPARES => false,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             ],
         ],
     ],
