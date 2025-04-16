fc7afbafabcc06d2fbe3258cc45e1b3718aa60c2
Enrique Cuevas Garcia <enrique.cuevas@iesdonana.org>
2025-04-16 13:38:28 +0200

    Actualizacion de profile y subida de imagenes

--- resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.jsx
+++ resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.jsx
@@ -7,7 +7,7 @@ export default function UpdateProfileInformation({ mustVerifyEmail, status, clas
     const [photoPreview, setPhotoPreview] = useState(null);
     const photoInput = useRef();
 
-    const { data, setData, post, errors, processing, recentlySuccessful } = useForm({
+    const { data, setData, patch, errors, processing, recentlySuccessful } = useForm({
         name: user.name,
         email: user.email,
         photo: null,
@@ -15,7 +15,7 @@ export default function UpdateProfileInformation({ mustVerifyEmail, status, clas
 
     const submit = (e) => {
         e.preventDefault();
-        post(route('profile.update'), {
+        patch(route('profile.update'), {
             preserveScroll: true,
             onSuccess: () => {
                 photoInput.current.value = '';
@@ -145,7 +145,7 @@ export default function UpdateProfileInformation({ mustVerifyEmail, status, clas
                 <div className="flex items-center gap-4">
                     <button
                         type="submit"
-                        className="inline-flex items-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150"
+                        className="inline-flex items-center px-4 py-2 bg-green-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-green-500 active:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 transition ease-in-out duration-150"
                         disabled={processing}
                     >
                         Guardar
