8ea20fb708fcfb9dfb99c047084a2528c2139c81
Enrique Cuevas Garcia <enrique.cuevas@iesdonana.org>
2025-04-22 10:50:26 +0200

    Solucion de problemas

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,25 @@
-module.exports = {
+export default {
     testEnvironment: 'jsdom',
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
     moduleNameMapper: {
         '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
-        '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+        '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
     },
     transform: {
-        '^.+\\.(js|jsx)$': 'babel-jest'
+        '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', {
+            presets: [
+                ['@babel/preset-env', { targets: { node: 'current' } }],
+                '@babel/preset-react',
+                '@babel/preset-typescript'
+            ]
+        }]
     },
     testMatch: [
-        '<rootDir>/resources/js/**/__tests__/**/*.{js,jsx}',
-        '<rootDir>/resources/js/**/*.{spec,test}.{js,jsx}'
+        '<rootDir>/resources/js/**/__tests__/**/*.{js,jsx,ts,tsx}',
+        '<rootDir>/resources/js/**/*.{spec,test}.{js,jsx,ts,tsx}'
+    ],
+    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
+    transformIgnorePatterns: [
+        '/node_modules/(?!(axios|@inertiajs)/)'
     ]
 }; 
--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,25 +1,17 @@
+import './bootstrap';
 import '../css/app.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../css/workouts.css';
 
-import { createInertiaApp } from "@inertiajs/react";
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
+import { createInertiaApp } from '@inertiajs/react';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) =>
-        resolvePageComponent(
-            `./Pages/${name}.jsx`,
-            import.meta.glob('./Pages/**/*.jsx'),
-        ),
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
     setup({ el, App, props }) {
         const root = createRoot(el);
-        root.render(<App {...props} />);  // Asegúrate de pasar las props correctamente
-    },
-    progress: {
-        color: '#4B5563',
+        root.render(<App {...props} />);
     },
 });
