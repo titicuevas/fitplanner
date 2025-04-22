3a4ff6f0bde7ec73a2b108884fe7683b0fa01096
Enrique Cuevas Garcia <enrique.cuevas@iesdonana.org>
2025-04-22 13:01:19 +0200

    Actualizacion del fichero

--- resources/js/Components/Modal.jsx
+++ resources/js/Components/Modal.jsx
@@ -1,3 +1,4 @@
+import { Fragment } from 'react';
 import {
     Dialog,
     Transition,
@@ -25,7 +26,7 @@ export default function Modal({
     }[maxWidth];
 
     return (
-        <Transition show={show} leave="duration-200">
+        <Transition appear show={show} as={Fragment}>
             <Dialog
                 as="div"
                 id="modal"
@@ -33,6 +34,7 @@ export default function Modal({
                 onClose={close}
             >
                 <Transition.Child
+                    as={Fragment}
                     enter="ease-out duration-300"
                     enterFrom="opacity-0"
                     enterTo="opacity-100"
@@ -44,6 +46,7 @@ export default function Modal({
                 </Transition.Child>
 
                 <Transition.Child
+                    as={Fragment}
                     enter="ease-out duration-300"
                     enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                     enterTo="opacity-100 translate-y-0 sm:scale-100"
